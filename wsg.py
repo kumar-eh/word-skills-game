cddbf52dca86a7f1501bd0555f8dfef96bf3a9fa
kumar-eh <68334628+kumar-eh@users.noreply.github.com>
2020-08-10 23:41:14 +0530

    timer functionality added

--- wsg.py
+++ wsg.py
@@ -1,27 +1,20 @@
 from tkinter import *
 import random
 import sys
+import tkinter.messagebox
 from tkinter.messagebox import *
-
-counter = 60                                                                                                            #for time#
-
-
-
+lists = []
+counter = 60
 global e
+global shuffled
 
-Score =0                                                                                                                #starting score is 0#
 
-file = open("files.txt" , "r+")
+# starting score is 0 #
+Score = 0 
+
+file = open('dictionaryWords.txt', 'r+')
 
 root = Tk()
-
-
-def repeat_words():
-    
-
-        else:
-            print(showwarning("alert" , "Word already entered"))
-    
 
 def counter_score(s):
     def counters():
@@ -31,151 +24,164 @@ def counter_score(s):
     counters()
 
 
-
-
 def three_letter(s):
     global Score
-    Score+=5
-    s.config(text = str(Score))
-
+    Score += 5
+    s.config(text=str(Score))
 
 
 def four_letter(s):
     global Score
-    Score+=10
-    s.config(text = str(Score))
+    Score += 10
+    s.config(text=str(Score))
 
 
-    
 def five_letter(s):
     global Score
-    Score+=20
-    s.config(text = str(Score))
-
+    Score += 20
+    s.config(text=str(Score))
 
 
 def six_letter(s):
     global Score
-    Score+=100
-    s.config(text = str(Score))
+    Score += 100
+    s.config(text=str(Score))
 
-
-
-
-
-
-
-def answer(e):                                                                                                           #when button b is clicked this is compiled#
-    l= [] 
-    global Score
-    global words
-    if(len(e) <3 or len(e) > 6):
-        print(showwarning("Warning" , "Minimum number of letters should be three and maximum should be six"))
+# when button b is clicked this is compiled #
+def answer(e): 
+    global l
+    if len(e) < 3 or len(e) > 6:
+        print(showwarning('Warning', 'Minimum number of letters should be three and maximum should be six'))
     else:
-        if(len(e) == 3):
-            with open("files.txt", "r") as read_obj:
+        if len(e) == 3:
+            with open('dictionaryWords.txt', 'r') as read_obj:
                 for line in read_obj:
                     if e.casefold() in line.casefold():
-                        repeat_words()
-                        three_letter(s)
+                        if e not in lists:
+                            lists.append(e)
+                            three_letter(s)
+                            return
+                        else:
+                            print(showwarning("Alert" , "Word already entered"))
+                            return
 
-
-                   
-        if(len(e) == 4):
-            with open("files.txt", "r") as read_obj:
+        if len(e) == 4:
+            with open('dictionaryWords.txt', 'r') as read_obj:
                 for line in read_obj:
                     if e.casefold() in line.casefold():
-                        four_letter(s)
-                        return
+                        if e not in lists:
+                            lists.append(e)
+                            four_letter(s)
+                            return
+                        else:
+                            print(showwarning("Alert" , "Word already entered"))
+                            return
+
+                        
 
 
-                
-        if(len(e) == 5):
-            with open("files.txt", "r") as read_obj:
+        if len(e) == 5:
+            with open('dictionaryWords.txt', 'r') as read_obj:
                 for line in read_obj:
                     if e.casefold() in line.casefold():
-                        five_letter(s)
-                        return
+                        if e not in lists:
+                            lists.append(e)
+                            five_letter(s)
+                            return
+                        else:
+                            print(showwarning("Alert" , "Word already entered"))
+                            return
 
 
-                
-        if(len(e) == 6):
-            with open("files.txt", "r") as read_obj:
+
+        if len(e) == 6:
+            with open('dictionaryWords.txt', 'r') as read_obj:
                 for line in read_obj:
                     if e.casefold() in line.casefold():
-                        six_letter(s)
-                        return
+                        if e not in lists:
+                            lists.append(e)
+                            six_letter(s)
+                            return
+                        else:
+                            print(showwarning("ALert" , "Word already entered"))
+                            return
+
+
         else:
-            print(showwarning("Warning" , "Please enter a correct word"))
+            print(showwarning('Warning', 'Please enter a correct word'))
             return
-            
 
 
 
-def counter_label(timer):                                                                                               #for timer function definition#
-  def count():
-    global counter
-    counter -= 1
-    timer.config(text=str(counter))
-    timer.after(1000, count)
-  count()
+def counter_label(timer):
+    def count():
+        global counter
+        global Score
+        if(counter > 0):
+            counter-=1
+            timer.config(text=str(counter))
+            timer.after(1000, count)
 
+        else:
+            result = tkinter.messagebox.showinfo("Game over","Press yes to exit")
+            if(result == True):
+                root.destroy()
+            else:
+                root.destroy()         
+    count()
 
-
-def question(word):                                                                                                     #for choosing a random a six letter word form the file#
-    while(True):
-        words = random.choice(open("filen.txt").read().split()).strip()
-        if(len(words) == 6):
+    
+# for choosing a random a six letter word form the file#
+def question(word):  
+    while True:
+        words = random.choice(open('dictionaryWords.txt').read().split()).strip()
+        if len(words) == 6:
             shuffled = list(words)
             random.shuffle(shuffled)
             shuffled = ''.join(shuffled)
-            word.config(text = str.upper(shuffled))
+            word.config(text=str.upper(shuffled))
             break
 
+# score label#
+l = Label(root, text='Score')  
+l.pack(side='top')
 
-        
-l = Label(root , text = "Score")                                                                                        #score label#
-l.pack(side = "top")
 
-
-s = Label(root , text =Score)                                                                                           #label for changing score#
+# label for changing score#
+s = Label(root, text=Score)  
 s.pack()
 counter_score(s)
 
+root.title('Game')
 
-root.title("Game")
-
-
-
-c = Canvas(root , height = 600 , width = 400)
+c = Canvas(root, height=600, width=400)
 c.pack()
 
-timer = Label(root, fg="green")                                                                                         #timer widget#
+
+# timer widget#
+timer = Label(root, fg='green' , font = ("Helvetica", 25))  
 timer.pack()
 counter_label(timer)
 
 
-f  = Frame(root , bg = "#4eedd3")                                                                                       #frame within the canvas#
-f.place(relx = 0.1 , rely = 0.1 , relwidth = 0.8 , relheight = 0.8)
+# frame within the canvas#
+f = Frame(root, bg='#03dac5')  
+f.place(relx=0.10, rely=0.10, relwidth=0.8, relheight=0.8)
 
 
-e = Entry(f , bg = "#3da4f2")                                                                                           #USER ENTRIED WORD#
-e.place(relx = 0.5 , rely = 0.1 , relheight = 0.10 , relwidth = 0.4 , anchor = "n")
+# USER ENTRIED WORD#
+e = Entry(f, bg='#3da4f2')  
+e.place(relx=0.5, rely=0.10, relheight=0.10, relwidth=0.4, anchor='n')
 
 
-
-b = Button(f , bg = '#ff0000' , text = "Submit"  , command = lambda : answer(e.get()))                                  #for entering after typing the word#
-b.pack(side = "bottom")
-
-
+# for entering after typing the word#
+b = Button(f, bg='#ff0000', text='Submit', command=lambda : answer(e.get()))  
+b.pack(side='bottom')
 
 
-
-word = Label(f , bg = "#3da4f2")                                                                                        #for displaying the six letter word#
-word.place(relx = 0.5 , rely = 0.9 , relheight = 0.10 , relwidth = 0.4 , anchor = "s")
+# for displaying the six letter word#
+word = Label(f, bg='#3da4f2' , fg = "#ffffff")  
+word.place(relx=0.5, rely=0.9, relheight=0.10, relwidth=0.4, anchor='s')
 question(word)
-
-
-
 
 root.mainloop()
